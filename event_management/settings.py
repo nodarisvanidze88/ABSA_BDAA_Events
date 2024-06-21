cd3ec63382c8707c8d1e65f1bc0e4ca8848ab824
root <root@DESKTOP-41JA4US>
2024-06-21 13:15:36 +0400

    change statics

--- event_management/settings.py
+++ event_management/settings.py
@@ -145,5 +145,30 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
-STATIC_URL = f'https://{os.getenv("BOTO_NAME")}.{os.getenv("BOTO_ENDPOINT")}/{os.getenv("AWS_LOCATION")}/'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/media/'
+# STATIC_URL = f'https://{os.getenv("BOTO_NAME")}.{os.getenv("BOTO_ENDPOINT")}/{os.getenv("AWS_LOCATION")}/'
+# MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/media/'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+ ]
+
+MEDIA_ROOT = BASE_DIR / 'static/media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "location": "static",
+        },
+    },
+}
